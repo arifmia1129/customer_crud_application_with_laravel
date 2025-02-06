7ed3a422a23b18f3a91abb2f8a0e139ad221631c
Md. Arif <arifmia1129@gmail.com>
2025-02-07 00:34:51 +0600

    CRUD - Working with Create Feature

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\CustomerController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('customer.index');
 });
+
+
+Route::resource('customers', CustomerController::class);
